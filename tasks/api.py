34edb44f8d2ff8c07ed39e04ab2c9e2119846b79
aurelio-mont <aurelio.mon@me.comt>
2024-02-29 14:28:05 -0600

    first commit

---
+++ tasks/api.py
@@ -0,0 +1,17 @@
+from rest_framework import viewsets, permissions, status
+from .models import Task
+from .serializers import TaskSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = TaskSerializer
+    
+    @action(detail=True, methods=['post'] )
+    def done(self, request, pk=None ):
+        task = self.get_object()
+        task.done = not task.done
+        task.save()
+        return Response({'status' : 'Task Done' if task.done else 'Task Undone' }, status=status.HTTP_200_OK)
